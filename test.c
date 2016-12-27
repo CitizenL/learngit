e2452ac8583594ff827be332ee32776e52e8c528
root <root@localhost.localdomain>
2016-12-28 05:05:06 +0800

    My copy

--- test.c
+++ test.c
@@ -1,48 +1,15 @@
-/*My copy
- *The format is ./copy oldfile newfile
- * */
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <unistd.h>
 #include "tlpi_hdr.h"
+#define MAX_READ 20
 
-#ifndef BUF_SIZE
-#define BUF_SIZE 1024
-#endif
-
-int main(int argc, char *argv[])
+int main(void)
 {
-    int inputFd, outputFd, openFlags;
-    mode_t filePerms;
+    char buffer[MAX_READ+1];
     ssize_t numRead;
-    char buf[BUF_SIZE];
-
-    if (argc != 3 || strcmp(argv[1], "--help") == 0)
-	usageErr("%s old-file new-file\n", argv[0]);
-    /* Open input and output files */
-
-    inputFd = open(argv[1], O_RDONLY );
-    if (inputFd == -1)
-	errExit("opening file %s", argv[1]);
-
-    openFlags = O_CREAT | O_WRONLY | O_TRUNC;
-    filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
-    outputFd = open(argv[2],openFlags, filePerms);
-    if (outputFd == -1)
-	errExit("opening file %s", argv[2]);
-
-    /* Transfer data until we encounter end of input or an error */ 
-
-    while ((numRead = read(inputFd, buf, BUF_SIZE)) > 0)
-	if (write(outputFd, buf, numRead) != numRead)
-	    fatal("couldn't write whole buffer");
+    numRead = read(STDIN_FILENO,buffer,MAX_READ);
     if (numRead == -1)
 	errExit("read");
-
-    if (close(inputFd) == -1)
-	errExit("close input");
-    if (close(outputFd) == -1)
-	errExit("close output");
-
-    exit(EXIT_SUCCESS);
+    buffer[numRead] = '\0';
+    printf("The input data was: %s",buffer);
     return 0;
 }
