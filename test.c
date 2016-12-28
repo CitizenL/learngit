c4e3e1dd0a2ae461c8022f346513cfc226f68528
root <root@localhost.localdomain>
2016-12-28 21:51:03 +0800

    My tee

--- test.c
+++ test.c
@@ -1,15 +1,29 @@
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
-#include "tlpi_hdr.h"
-#define MAX_READ 20
+#define BUF_SIZE 1024
+extern int optind;
+int getopt(int argc, char *argv[],const char *optstring);
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    char buffer[MAX_READ+1];
-    ssize_t numRead;
-    numRead = read(STDIN_FILENO,buffer,MAX_READ);
-    if (numRead == -1)
-	errExit("read");
-    buffer[numRead] = '\0';
-    printf("The input data was: %s",buffer);
+    char *pstr;
+    char buf[BUF_SIZE+1];
+    int fd,openFlags,cnt;
+    int opt;
+
+    openFlags =  O_WRONLY | O_CREAT | O_TRUNC;
+    while((opt = getopt(argc,argv,":a")) != -1){
+	switch (opt){
+	    case 'a' : openFlags = O_WRONLY | O_CREAT | O_APPEND; break;
+	    default : openFlags =  O_WRONLY | O_CREAT | O_TRUNC; break; 
+	}
+    }
+    if(optind < argc)
+	fd = open(argv[optind],openFlags,S_IWUSR | S_IRUSR | S_IWGRP | S_IRGRP | S_IWOTH | S_IROTH);
+    while( (cnt = read(STDIN_FILENO,buf,BUF_SIZE)) != 0){
+	write(fd,buf,cnt);
+	//printf("Write %d chunks\n",cnt);
+	}
     return 0;
 }
